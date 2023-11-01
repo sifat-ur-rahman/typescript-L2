13d587e9586772c9ef8bb835c6772fee96896656
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-01 22:25:17 +0600

    add

---
+++ typescript-D1/dist/basicDataType.js
@@ -0,0 +1,12 @@
+"use strict";
+let firstName = 'sifat'; //string
+let rollNumber = 50; //number 
+let isAdmin = false; //boolean
+let x = undefined; // undefined
+let y = null; //null
+let d;
+d = 520;
+//Array 
+let friends = ['putul', 'sifat'];
+//tuple --> array --> order --> type of values
+let ageName = [50, 'mir'];
