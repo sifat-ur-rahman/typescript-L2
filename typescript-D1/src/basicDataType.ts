deb94a9b76b6f469233caf97c80ebee8130e4f9f
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-01 21:20:29 +0600

    add

--- typescript-D1/src/basicDataType.ts
+++ typescript-D1/src/basicDataType.ts
@@ -1,5 +1,21 @@
 
 let firstName:string = 'sifat'  //string
 let rollNumber : number = 50  //number 
+let isAdmin : boolean = false  //boolean
+let x : undefined = undefined  // undefined
+let y : null = null             //null
 
 
+let d : number
+
+d=520
+
+
+//Array 
+
+let friends : string[]= ['putul', 'sifat']
+
+
+//tuple --> array --> order --> type of values
+
+let ageName :[number, string]  = [50, 'mir']
