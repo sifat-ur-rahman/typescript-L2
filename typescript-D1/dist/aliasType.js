80febcfacb7e38e2a42f1bb947a161bad8403fa9
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-02 11:33:06 +0600

    add

---
+++ typescript-D1/dist/aliasType.js
@@ -0,0 +1,21 @@
+"use strict";
+{
+    //object
+    const student1 = {
+        id: 256,
+        name: '',
+        age: 25,
+        gender: 'male',
+        address: 'khulna'
+    };
+    const student2 = {
+        id: 256,
+        name: '',
+        age: 25,
+        gender: 'male',
+        address: 'khulna'
+    };
+    //function
+    const add = (num1, num2) => num1 + num2;
+    //
+}
---
+++ typescript-D1/src/aliasType.ts
@@ -0,0 +1,46 @@
+{
+//
+//Alias Type
+type Student ={
+    id: number;
+    name: string;
+    age: number;
+    gender: string;
+    address: string
+}
+
+
+//object
+const student1:Student={
+    id:256,
+    name:'',
+    age: 25,
+    gender: 'male',
+    address: 'khulna'
+}
+
+const student2:Student ={
+    id:256,
+    name:'',
+    age: 25,
+    gender: 'male',
+    address: 'khulna'
+}
+
+
+//Alias Type
+type Add = (x:number, y:number) => number
+
+
+//function
+const add:Add = (num1, num2) => num1+num2
+
+
+
+
+
+
+
+
+//
+}
