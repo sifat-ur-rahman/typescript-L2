3b6bc3ea0adcfa291a461ec7b5f7c8417daff6ad
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-02 11:44:13 +0600

    add

---
+++ typescript-D1/dist/unionType.js
@@ -0,0 +1,11 @@
+"use strict";
+{
+    const newDev = "goodDev";
+    const user1 = {
+        name: 'Putul',
+        email: 'putul@gmail.com',
+        gender: 'female',
+        bloodGroup: 'AB+'
+    };
+    //
+}
---
+++ typescript-D1/src/unionType.ts
@@ -0,0 +1,37 @@
+{
+    //union types
+
+type FrontendDev = 'badDev'|'goodDev'
+
+const newDev:FrontendDev = "goodDev"
+
+
+type User = {
+    name : string;
+    email: string;
+    gender : "male"|"female";
+    bloodGroup: "A+"|"O+"|"B+"|"AB+"
+}
+
+const user1 : User={
+    name:'Putul',
+    email:'putul@gmail.com',
+    gender: 'female',
+    bloodGroup: 'AB+'
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//
+}
+
