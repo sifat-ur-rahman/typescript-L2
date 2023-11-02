c6659ae3edb24d48e09beaa4dd3117c50ffc1833
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-02 08:55:48 +0600

    add

--- typescript-D1/dist/destructuring.js
+++ typescript-D1/dist/destructuring.js
@@ -1,3 +1,14 @@
 "use strict";
 {
+    //destructuring
+    const user = {
+        home: 'bangladesh',
+        name: {
+            firstName: 'sifat',
+            lastName: 'Rahman',
+        },
+        isMarried: false,
+        contactNo: '12800232'
+    };
+    const { contactNo, name: { lastName } } = user;
 }
--- typescript-D1/src/destructuring.ts
+++ typescript-D1/src/destructuring.ts
@@ -1,3 +1,33 @@
 {
+//destructuring
+
+const user  = {
+    home: 'bangladesh',
+    name:{
+        firstName : 'sifat',
+        lastName : 'Rahman',
+    },
+    isMarried: false,
+    contactNo:'12800232'
     
+}
+
+
+const {contactNo ,name:{lastName} }=user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
