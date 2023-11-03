1aa9e4e2f582d3d7fad1baf2a47c277ec16e3708
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-03 20:45:16 +0600

    add

--- typescript-D2/src/interfaceType.ts
+++ typescript-D2/src/interfaceType.ts
@@ -7,11 +7,32 @@
         age: number
     }
 
-    const user : User ={
+
+    interface UserWithRole extends User { role ?: string}
+
+    const user : UserWithRole ={
         name : 'sifat',
         email: 'sifat@gmail.com',
-        age: 24
+        age: 24,
+        role: 'student'
     }
+
+// array interface
+
+    interface Roll {
+        [index : number] : number
+    }
+
+    const rollNumber : Roll =[1,2,3]
+
+// function interface
+    interface Add {
+        (num1:number, num2:number):number
+    }
+
+    const add :Add =(num1, num2) => num1+num2
+
+
 
     //
 }
