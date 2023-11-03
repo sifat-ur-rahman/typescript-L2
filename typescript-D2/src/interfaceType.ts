380ababe1e8f07b0e61cc9d8a377256265da93ea
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-03 20:31:36 +0600

    add

---
+++ typescript-D2/src/interfaceType.ts
@@ -0,0 +1,17 @@
+{
+    //interface
+
+    interface User {
+        name: string;
+        email: string;
+        age: number
+    }
+
+    const user : User ={
+        name : 'sifat',
+        email: 'sifat@gmail.com',
+        age: 24
+    }
+
+    //
+}
