b0df1de6bd295ec3c494fefb687b83f3c4c09c9e
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-03 21:12:20 +0600

    add

---
+++ typescript-D2/src/genericType.ts
@@ -0,0 +1,21 @@
+{
+    // generic Type
+
+    type GenericArray<T> = Array<T>
+
+    const rollNumber: Array<number> = [2, 4, 5]
+
+    const rollNumber2: GenericArray<number> = [2, 4, 5]
+
+
+    const name : Array<string> = ['sifat','putul']
+    const name2 : GenericArray<string> = ['sifat','putul']
+
+
+    
+
+
+
+
+    //
+}
